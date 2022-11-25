07000c9e5990747581a953fe5f437e51d595eadc
Franco Monzon <monzonfranco95@gmail.com>
2022-11-25 03:59:13 -0300

    Agregue contenido dinamico al Modal

--- app.js
+++ app.js
@@ -65,9 +65,11 @@ Swal.fire({
     
     const petalos = 4 * figura + 2;
     document.getElementById("resultado");
-    modalContent.innerHTML='<p>soy spiderman</p>';    
+    // modalContent.innerHTML='<p>La La figura N° '+ figura + ' va a tener  '+ petalos +' mosaicos</p>';    
+    modalContent.innerHTML='<p>M = Número de Mosaicos <br>F = Figura <br><br><b>Datos:</b><table class="table"><thead><tr><th scope="col">Figura</th><th scope="col">Mosaicos</th><th scope="col">Patrón</th></tr></thead><tbody><tr><th scope="row">1</th><td>6</td><td>4 . 1 + 2</td></tr><tr><th scope="row">2</th><td>10</td><td>4 . 2 + 2</td></tr><tr><th scope="row">3</th><td >14</td><td>4 . 3 + 2</td></tr></tbody></table> <br><b>M ( f ) = 4 . f + 2</b><br><br>  Tu número de figura es <b>'+ figura +'</b> entonces...<br>     <b>4 . '+ figura +' + 2 = '+ petalos +'</b><br><br><b>Tu figura tendra '+petalos+' mosaicos !</b></p>';
     myModal.show();
     console.log('I was closed by the timer');
+
     document.getElementById("figura").value='';
 
 // Swal.fire('La figura N° ' + figura + ' va a tener ' + petalos +' mosaicos');
